519f987eeb6d4f623bbb4f6f08af45a7eef74991
Tibi Neagu <cristi9810@gmail.com>
2020-03-05 10:55:34 +0200

    implemented infinite scroll, posts now load correctly sorted by date

--- front-app/src/home/Home.js
+++ front-app/src/home/Home.js
@@ -2,27 +2,74 @@ import React, { Component } from "react";
 
 import Grid from '@material-ui/core/Grid';
 import Axios from "axios";
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 import Post from '../components/post/Post.js';
-import { red } from "@material-ui/core/colors";
 
 
 class Home extends Component {
     state = {
         posts: [],
+        startPosition: 0,
+        step: 3,
+        hasMore: true
     }
 
     componentDidMount() {
-        let posts = [];
+        //get initial Posts
+        this.fetchInitialPosts();
+    }
 
-        //get new Posts
-        Axios.get('/posts/getAllPosts')
+    fetchInitialPosts() {
+        let startPosition = this.state.startPosition;
+        let step = this.state.step;
+
+        //get initial Posts
+        Axios.post('/users/getPostRecommendationsPaginated/2', null, {
+            params: {
+                startPosition: startPosition,
+                step: step
+            }
+        })
             .then((res) => {
-                posts = res.data
+                let posts = res.data
 
                 this.setState({
-                    posts: posts
+                    posts: posts,
                 });
+            })
+            .catch(err => console.log(err));
+    }
+
+    fetchNextPosts = () => {
+        let step = this.state.step;
+        let currentStartPosition = this.state.startPosition;
+
+        let newStartPosition = currentStartPosition + step;
+
+        //get new Posts
+        Axios.post('/users/getPostRecommendationsPaginated/2', null, {
+            params: {
+                startPosition: newStartPosition,
+                step: step
+            }
+        })
+            .then((res) => {
+                let posts = res.data
+
+                if (posts) {
+                    this.setState({
+                        //append new Posts
+                        posts: this.state.posts.concat(posts),
+                        startPosition: newStartPosition
+                    });
+                }else{
+                    this.setState({
+                        hasMore: false
+                    });
+
+                    return;
+                }
             })
             .catch(err => console.log(err));
     }
@@ -30,13 +77,15 @@ class Home extends Component {
     render() {
         let recentPostsMarkup = this.state.posts ? (
             this.state.posts.map(post => <Post post={post}></Post>)
-        ) : <p>Loading ...</p>
+        ) : <p></p>
 
         return (
             <Grid container spacing={10}>
-                <Grid item sm={8} xs={12}>
-                    {recentPostsMarkup}
-                    <p>Content ...</p>
+
+                <Grid item sm={5} xs={10}>
+                    <InfiniteScroll dataLength={this.state.posts.length} next={this.fetchNextPosts} hasMore={this.state.hasMore} loader={<h4>Loading ...</h4>} endMessage={<p><b>No more posts :(</b></p>}>
+                        {recentPostsMarkup}
+                    </InfiniteScroll>
                 </Grid>
                 <Grid item sm={4} xs={12}>
                     <p>Profile ...</p>
